6e578efe9699f885576582c739f4a66b7c4d1498
Ares-10 <cholionking1@naver.com>
2023-09-08 18:10:26 +0900

    first commit

---
+++ all-clear/src/main/java/AC/company/all/clear/AllClearApplication.java
@@ -0,0 +1,13 @@
+package AC.company.all.clear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllClearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllClearApplication.class, args);
+	}
+
+}
---
+++ all-clear/src/test/java/AC/company/all/clear/AllClearApplicationTests.java
@@ -0,0 +1,13 @@
+package AC.company.all.clear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllClearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
