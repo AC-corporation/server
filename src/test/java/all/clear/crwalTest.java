50432c0e3fcf6336bf60c58060f4fd39d7dc1f61
chanmin-00 <jjw421412@naver.com>
2023-11-18 02:12:07 +0900

    feat : 스프링 프로젝트 내 크롤링 기능 추가

---
+++ src/test/java/all/clear/crwalTest.java
@@ -0,0 +1,12 @@
+package all.clear;
+
+import all.clear.crwal.RequirementCrwal;
+import org.junit.jupiter.api.Test;
+
+public class crwalTest {
+    @Test
+    public static void main(String[] args) {
+        RequirementCrwal requirementCrwal = new RequirementCrwal();
+        requirementCrwal.crwalRequirementComponent("20203058","Jjw112233#");
+    }
+}
