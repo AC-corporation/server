0f12fe2fd5a656220b18951e639b0838811517d3
chanmin-00 <jjw421412@naver.com>
2023-11-19 20:44:54 +0900

    chore : crwal 클래스명 수정 및 코드 수정

---
+++ src/main/java/all/clear/crwal/CrwalUserInfo.java
@@ -0,0 +1,101 @@
+package all.clear.crwal;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class CrwalUserInfo {
+
+    ArrayList<String> userInfoList = new ArrayList<>();
+    ArrayList<String> requirementComponentList = new ArrayList<>();
+    WebDriver driver;
+
+    public void loginUsaint(String usaintId, String usaintPassword){
+        System.setProperty("ENCODING", "UTF-8");
+        WebDriverManager.chromedriver().setup();
+        // 로그인 페이지 주소
+        String loginUrl = "https://smartid.ssu.ac.kr/Symtra_sso/smln.asp?apiReturnUrl=https%3A%2F%2Fsaint.ssu.ac.kr%2FwebSSO%2Fsso.jsp";
+        // 입력 받은 아이디와 비밀번호
+        String userId = usaintId;
+        String password = usaintPassword;
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        /*
+         *사용자 정보 획득을 위한 로그인
+         */
+        driver.get(loginUrl); // 로그인 접속
+        WebElement usernameElement = driver.findElement(By.name("userid"));
+        WebElement passwordElement = driver.findElement(By.name("pwd")); // 로그인 시도
+        usernameElement.sendKeys(userId);
+        passwordElement.sendKeys(password);
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"sLogin\"]/div/div[1]/form/div/div[2]/a"));
+        loginButton.click();
+        try {
+            Thread.sleep(1000); // 1초 동안 실행을 멈추기
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 학사관리 클릭
+        WebElement degreeManagebutton = driver.findElement(By.xpath("//*[@id=\"ddba4fb5fbc996006194d3c0c0aea5c4\"]/a"));
+        degreeManagebutton.click();
+    }
+    public void crwalRequirementComponent(){
+        // 성적/졸업 버튼 클릭
+        WebElement gradeAndGraduationButton = driver.findElement(By.xpath("//*[@id=\"8d3da4feb86b681d72f267880ae8cef5\"]"));
+        gradeAndGraduationButton.click();
+        Duration timeout = Duration.ofSeconds(10);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        // 졸업사정표 버튼 클릭
+        WebElement graduationRequirementButton = driver.findElement(By.xpath("//*[@id=\"30f2303171c98bdf57db799d0b834646\"]/a"));
+        graduationRequirementButton.click();
+        try {
+            Thread.sleep(1000); // 1초 동안 실행을 멈추기
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement iframe1Element = driver.findElement(By.name("contentAreaFrame"));
+        driver.switchTo().frame(iframe1Element);
+        WebElement iframe2Element = driver.findElement(By.name("isolatedWorkArea"));
+        driver.switchTo().frame(iframe2Element);
+        try {
+            Thread.sleep(5000); // 1초 동안 실행을 멈추기
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        /*
+         * 졸업요건 정보 획득
+         */
+        int exit_flag = 0;
+        int i = 2;
+        String targetRoot;
+        WebElement target;
+        String text;
+        while (true){
+            for (int j=1;j<=6;j++){
+                targetRoot = "/html/body/table/tbody/tr/td/div/table/tbody/tr/td/div/table/tbody/tr[3]/td/" +
+                        "table/tbody[2]/tr/td/div/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr["+i+"]/td["+j+"]";
+                try {
+                    target = driver.findElement(By.xpath(targetRoot));
+                    text = target.getText().strip();
+                    if (text.equals("채플"))
+                        exit_flag = 1;
+                    requirementComponentList.add(text);
+                }
+                catch (Exception e) {
+                    continue;
+                }
+            }
+            if(exit_flag ==1)
+                break;
+            i = i+1;
+        }
+        for(int j=0;j<requirementComponentList.size();j++)
+            System.out.println(requirementComponentList.get(j));
+    }
+}
+
--- src/test/java/all/clear/crwalTest.java
+++ src/test/java/all/clear/crwalTest.java
@@ -1,12 +1,12 @@
 package all.clear;
 
-import all.clear.crwal.RequirementCrwal;
+import all.clear.crwal.CrwalUserInfo;
 import org.junit.jupiter.api.Test;
 
 public class crwalTest {
     @Test
     public static void main(String[] args) {
-        RequirementCrwal requirementCrwal = new RequirementCrwal();
-        requirementCrwal.crwalRequirementComponent("20203058","Jjw112233#");
+        CrwalUserInfo crwalUserinfo = new CrwalUserInfo();
+        crwalUserinfo.loginUsaint("20203058","aksakstp1@");
     }
 }
