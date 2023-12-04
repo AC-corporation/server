7aa3e758e706de2ceb57324101c22042a1391042
daeun084 <030804jk@naver.com>
2023-12-04 19:09:34 +0900

    feat: signup dto, cotroller, service 함수 구현

--- src/main/java/all/clear/controller/UserController.java
+++ src/main/java/all/clear/controller/UserController.java
@@ -1,11 +1,15 @@
 package all.clear.controller;
 
+import all.clear.dto.requestDto.LoginRequesetDto;
+import all.clear.dto.requestDto.UserSignupRequestDto;
+import all.clear.global.response.ApiResponse;
 import all.clear.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.service.ResponseMessage;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,9 +17,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    //회원가입
+    @Operation(summary = "회원가입", description = "회원가입")
+    @PostMapping("signup")
+    public ApiResponse signup(@RequestBody UserSignupRequestDto userSignupRequestDto){
+        userService.createUser(userSignupRequestDto);
+        return ApiResponse.onSuccess("");
+    }
 
 
-    //@Operation(summary = "로그아웃", description = "로그아웃")
+    //로그읜
+    @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인")
+    public ApiResponse login(@RequestBody LoginRequesetDto loginRequesetDto){
+        userService.login(loginRequesetDto);
+        return ApiResponse.onSuccess("");
+    }
+
+
+
+    @Operation(summary = "로그아웃", description = "로그아웃")
     @GetMapping("/logout")
     public void logout(){
 
