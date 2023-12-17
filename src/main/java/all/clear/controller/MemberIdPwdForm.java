57a2efdbd79874030b2c4f5dc70ed6c89e8d1fa2
Ares-10 <cholionking1@naver.com>
2023-12-17 23:01:45 +0900

    refactor: User에서 Member로 리펙토링

--- src/main/java/all/clear/controller/MemberIdPwdForm.java
+++ src/main/java/all/clear/controller/MemberIdPwdForm.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
-public class UserIdPwdForm {
+public class MemberIdPwdForm {
     @NotEmpty(message = "유세인트 아이디와 비밀번호는 필수입니다")
     private Long usaintId;
     private String usaintPassword;
