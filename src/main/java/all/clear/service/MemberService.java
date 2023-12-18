ef37dbf8373a01721a6b4f3e7f3793aab6b55472
Ares-10 <cholionking1@naver.com>
2023-12-19 01:39:06 +0900

    refactor: Crwal -> Crawl

--- src/main/java/all/clear/service/MemberService.java
+++ src/main/java/all/clear/service/MemberService.java
@@ -1,11 +1,15 @@
 package all.clear.service;
 
+import all.clear.crwal.CrawlMemberInfo;
 import all.clear.domain.Member;
+import all.clear.domain.grade.Grade;
+import all.clear.domain.requirement.Requirement;
+import all.clear.repository.GradeRepository;
 import all.clear.repository.MemberRepository;
+import all.clear.repository.RequirementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,45 @@ public class MemberService {
     @Autowired
     private final MemberRepository memberRepository;
 
+    @Autowired
+    private final RequirementRepository requirementRepository;
 
+    @Autowired
+    private final GradeRepository gradeRepository;
+
+    public Member findOne(Long id) {
+        return memberRepository.findById(id).get();
+    }
+
+    /**
+     * 유저 정보 업데이트
+     */
+    // 크롤링 실패했을 때 처리 추가 필요
+    // 유저 정보 없을 때 처리 추가 필요
+    public void update(Long id, int usaintId, String usaintPasswd){
+        Member member = findOne(id);
+        CrawlMemberInfo crawlInfo = new CrawlMemberInfo(usaintId, usaintPasswd);
+
+        //멤버 초기화
+        Member newMember = crawlInfo.getMember();
+        member.setMemberName(newMember.getMemberName());
+        member.setUniversity(newMember.getUniversity());
+        member.setMajor(newMember.getMajor());
+        member.setMail(newMember.getMail());
+        member.setClassType(newMember.getClassType());
+        member.setLevel(newMember.getLevel());
+        member.setSemester(newMember.getSemester());
+
+        //졸업요건 초기화
+        requirementRepository.delete(member.getRequirement());
+        Requirement newRequirement = crawlInfo.getRequirement();
+        newRequirement.setMember(member);
+        requirementRepository.save(newRequirement))
+
+        //성적 초기화
+        gradeRepository.delete(member.getGrade());
+        Grade newGrade = crawlInfo.getGrade();
+        newGrade.setMember(member);
+        gradeRepository.save(newGrade);
+    }
 }
