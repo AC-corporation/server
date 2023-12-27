6c14f4c181f67dea607d7a6816cb529ecd77861e
chanmin-00 <jjw421412@naver.com>
2023-12-27 14:45:00 +0900

    chore : ParsingRequirement 에러 처리

--- src/test/java/allclear/crawl/ParsingTest.java
+++ src/test/java/allclear/crawl/ParsingTest.java
@@ -22,7 +22,7 @@ public class ParsingTest {
         List<SemesterGrade> semesterGradeList = new ArrayList<>();
         List<SemesterSubject> semesterSubjectList = new ArrayList<>();
 
-        crawlMemberInfo = new CrawlMemberInfo("ID",  "PWD");
+        crawlMemberInfo = new CrawlMemberInfo("20223168",  "todo!9844");
         requirement = crawlMemberInfo.getRequirement();
         grade = crawlMemberInfo.getGrade();
 
