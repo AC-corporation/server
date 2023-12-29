33b85f4e943688a54593084ba0d9cd1ffe2f38a1
Ares-10 <cholionking1@naver.com>
2023-12-29 13:27:11 +0900

    chore: GetMapping 인자 수정

--- src/main/java/allclear/controller/GradeController.java
+++ src/main/java/allclear/controller/GradeController.java
@@ -5,6 +5,7 @@ import allclear.global.response.ApiResponse;
 import allclear.service.GradeService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,13 @@ public class GradeController {
     private final GradeService gradeService;
 
     @Operation(summary = "전체 성적 조회", description = "전체 성적 조회")
-    //@GetMapping("") // string 수정 필요
+    @GetMapping("/getGrade")
     public ApiResponse<GradeResponseDto> get(Long userId){ //인자 수정 필요
         return ApiResponse.onSuccess("전체 성적 조회에 성공했습니다",gradeService.getGrade(userId));
     }
 
     //학기별 성적 조회
-//    @GetMapping() //수정 필요
+//    @GetMapping("/getSemesterGrade") //수정 필요
 //    public ApiResponse<SemesterGradeResponseDto> getSemesterGrade(@PathVariable Long semesterGradeId){
 //        return ApiResponse.onSuccess();
 //        //SemesterGrade id로 찾아 반환
