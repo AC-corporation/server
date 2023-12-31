f1aba5434c7e6ba49f60f988c28d0c62b3cac57a
chanmin-00 <jjw421412@naver.com>
2023-12-31 16:05:34 +0900

    feat : CrawlSubjectInfo 과목 크롤링 기능 추가

---
+++ src/test/java/allclear/crawl/CrawlSubjectTest.java
@@ -0,0 +1,13 @@
+package allclear.crawl;
+
+import allclear.domain.member.Member;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CrawlSubjectTest {
+    @Test
+    public static void main(String[] args) {
+        CrawlSubjectInfo crawlSubjectInfo = new CrawlSubjectInfo("ID", "PWD");
+    }
+}
