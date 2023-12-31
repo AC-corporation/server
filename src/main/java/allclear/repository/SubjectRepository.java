c6235a1e693d7f40d32fd4e2c6ea6dae5a3e53ef
Ares-10 <cholionking1@naver.com>
2023-12-31 16:32:38 +0900

    add: 조회 함수 추가

--- src/main/java/allclear/repository/SubjectRepository.java
+++ src/main/java/allclear/repository/SubjectRepository.java
@@ -9,6 +9,9 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
     Subject findByName(String name); //이름으로 과목 찾기
     Subject findByProfessor(String professor); //교수명으로 과목 찾기
     Subject findByClassInfoListLike(String classInfo); //수업 정보에 특정 문자열을 포함하는 과목 찾기
+    Subject findByDepartment(String department); //개설학과로 과목 찾기
     Subject findByMajorClassification(String majorClassification); //이수구분(주전공)으로 과목 찾기
     Subject findByMultiMajorClassification(String multiMajorClassification); //이수구분(다전공)으로 과목 찾기
+    Subject findBySubjectTargetList(String subjectTarget); //수강 대상에 특정 문자열을 포함하는 과목 찾기
+    Subject findByCredit(Integer credit); //학점으로 과목 찾기
 }
