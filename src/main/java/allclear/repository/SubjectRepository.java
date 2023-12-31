4c5c30e5089cb646a7e9bb24abd329b8c5a28910
Ares-10 <cholionking1@naver.com>
2023-12-31 16:07:19 +0900

    feat: 강의, 시간표 리포지토리 구현

---
+++ src/main/java/allclear/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package allclear.repository;
+
+import allclear.domain.subject.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
