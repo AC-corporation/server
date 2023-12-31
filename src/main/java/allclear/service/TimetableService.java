50fe49e971edd5b4d5919e121c3a5d3a1ef8c315
Ares-10 <cholionking1@naver.com>
2023-12-31 17:21:34 +0900

    feat: 시간표 조회, 삭제 서비스 구현

--- src/main/java/allclear/service/TimetableService.java
+++ src/main/java/allclear/service/TimetableService.java
@@ -1,11 +1,28 @@
 package allclear.service;
 
+import allclear.domain.timetable.Timetable;
+import allclear.dto.responseDto.timetable.TimetableResponseDto;
 import allclear.repository.TimetableRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TimetableService {
     private final TimetableRepository timetableRepository;
+
+
+    public Timetable findOne(Long id) {
+        return timetableRepository.findById(id).get();
+    }
+
+    public TimetableResponseDto getTimetable(Long id) {
+        return new TimetableResponseDto(findOne(id));
+    }
+
+    @Transactional
+    public void deleteTimetable(Long id) {
+        timetableRepository.deleteById(id);
+    }
 }
