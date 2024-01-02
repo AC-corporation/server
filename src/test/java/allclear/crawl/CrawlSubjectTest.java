b46754418b21e805d0b2bde36d5f37d672b1a2df
chanmin-00 <jjw421412@naver.com>
2024-01-02 16:34:55 +0900

    chore : CrawlSubjectInfo 과목 크롤링 함수 추가

--- src/test/java/allclear/crawl/CrawlSubjectTest.java
+++ src/test/java/allclear/crawl/CrawlSubjectTest.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 public class CrawlSubjectTest {
     @Test
     public static void main(String[] args) {
+        ArrayList<String> majorSubjectList = new ArrayList<>();
         CrawlSubjectInfo crawlSubjectInfo = new CrawlSubjectInfo("ID", "PWD");
+        majorSubjectList = crawlSubjectInfo.getMajorSubjects();
+        for(int i=0;i<majorSubjectList.size();i++){
+            String majorSubject = majorSubjectList.get(i);
+            System.out.println(majorSubject);
+        }
+
     }
 }
