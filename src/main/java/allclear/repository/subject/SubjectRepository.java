e1fe4109bb431ac85ba37489dc0272632f713f99
Ares-10 <cholionking1@naver.com>
2024-01-07 19:23:00 +0900

    add: DB 조회 함수 추가

--- src/main/java/allclear/repository/subject/SubjectRepository.java
+++ src/main/java/allclear/repository/subject/SubjectRepository.java
@@ -4,6 +4,17 @@ import allclear.domain.subject.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    //개설 학과 기준 조회
+    List<Subject> findByDepartment(String department);
+    //이수 구분(주전공) 기준 조회
+    List<Subject> findByMajorClassification(String majorClassification);
+    //이수 구분(다전공) 기준 조회
+    List<Subject> findByMultiMajorClassification(String multiMajorClassification);
+    //수강 대상 기준 조회
+    List<Subject> findBySubjectTarget(String subjectTarget);
+
 }
