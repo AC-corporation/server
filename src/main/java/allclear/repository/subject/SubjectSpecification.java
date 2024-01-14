6650544762639f0de947ee367fe8eca4a3be1425
Ares-10 <cholionking1@naver.com>
2024-01-14 21:54:05 +0900

    chore: 과목 조건 검색 수정

--- src/main/java/allclear/repository/subject/SubjectSpecification.java
+++ src/main/java/allclear/repository/subject/SubjectSpecification.java
@@ -4,37 +4,46 @@ import allclear.domain.subject.ClassInfo;
 import allclear.domain.subject.Subject;
 import allclear.dto.requestDto.subject.GetSubjectListRequestDto;
 import jakarta.persistence.criteria.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
+@Builder
 public class SubjectSpecification {
     public static Specification<Subject> subjectFilter(GetSubjectListRequestDto requestDto) {
         return (root, query, cb) -> {
             List<Predicate> predicates = new ArrayList<>();
-            if (requestDto.getSubjectId() != null) {
-                predicates.add(cb.equal(root.get("subjectId"), requestDto.getSubjectId()));
+            Join<Subject, ClassInfo> classInfoJoin = root.join("classInfoList");
+
+            if (requestDto.getSearchString() != null && !requestDto.getSearchString().isEmpty()) {
+                String likeSearchString = "%" + requestDto.getSearchString() + "%";
+
+                predicates.add(cb.or(
+                        cb.like(root.get("subjectName"), likeSearchString),
+                        cb.like(root.get("department"), likeSearchString),
+                        cb.like(root.get("majorClassification"), likeSearchString),
+                        cb.like(root.get("multiMajorClassification"), likeSearchString),
+                        cb.like(root.get("engineeringCertification"), likeSearchString),
+                        cb.like(root.get("subjectTarget"), likeSearchString),
+                        cb.like(root.get("subjectId").as(String.class), likeSearchString),
+                        cb.like(root.get("credit").as(String.class), likeSearchString),
+                        cb.like(classInfoJoin.get("professor"), likeSearchString),
+                        cb.like(classInfoJoin.get("classDay"), likeSearchString)
+                ));
             }
-            if (requestDto.getSubjectName() != null) {
-                predicates.add(cb.like(root.get("subjectName"), "%" + requestDto.getSubjectName() + "%"));
-            }
-            if (requestDto.getProfessor() != null) {
-                Join<Subject, ClassInfo> classInfoJoin = root.join("classInfoList");
-                predicates.add(cb.like(classInfoJoin.get("professor"), "%" + requestDto.getProfessor() + "%"));
-            }
-            if (requestDto.getDepartment() != null) {
-                predicates.add(cb.like(root.get("department"), "%" + requestDto.getDepartment() + "%"));
-            }
-            if (requestDto.getMajorClassification() != null) {
-                predicates.add(cb.like(root.get("majorClassification"), "%" + requestDto.getMajorClassification() + "%"));
-            }
-            if (requestDto.getCredit() != null) {
-                predicates.add(cb.equal(root.get("credit"), requestDto.getCredit()));
-            }
-            if (requestDto.getSubjectTarget() != null) {
+
+            if (requestDto.getSubjectTarget() != null && !requestDto.getSubjectTarget().isEmpty()) {
                 predicates.add(cb.like(root.get("subjectTarget"), "%" + requestDto.getSubjectTarget() + "%"));
             }
+
+            if (requestDto.getYear() != null && !requestDto.getYear().isEmpty()) {
+                predicates.add(cb.like(root.get("year"), "%" + requestDto.getYear() + "%"));
+            }
+
             return cb.and(predicates.toArray(new Predicate[0]));
         };
     }
