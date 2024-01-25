1f8ed8f0f2a1edc16a5548020a34a84fab425e85
Ares-10 <cholionking1@naver.com>
2024-01-25 17:12:36 +0900

    add: Step7,8RequestDto 추가 및 구정

--- src/main/java/allclear/dto/requestDto/timetableGenerator/Step7RequestDto.java
+++ src/main/java/allclear/dto/requestDto/timetableGenerator/Step7RequestDto.java
@@ -1,14 +1,14 @@
 package allclear.dto.requestDto.timetableGenerator;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
-public class Step8RequestDto {
-    @NotBlank
-    Long timetableGeneratorTimetableId;
+public class Step7RequestDto {
+    List<Long> timetableGeneratorSubjectIdList;
 }
