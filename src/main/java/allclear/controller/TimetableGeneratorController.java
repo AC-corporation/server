38dacee5cd8be3a76b0415c90bfe49002f522d04
Ares-10 <cholionking1@naver.com>
2024-02-11 21:17:39 +0900

    refactor: 시간표 생성기 파일 분할

--- src/main/java/allclear/controller/TimetableGeneratorController.java
+++ src/main/java/allclear/controller/TimetableGeneratorController.java
@@ -2,7 +2,7 @@ package allclear.controller;
 
 import allclear.dto.requestDto.timetableGenerator.*;
 import allclear.global.response.ApiResponse;
-import allclear.service.TimetableGeneratorManager;
+import allclear.service.timetableGenerator.TimetableGeneratorService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/timetableGenerator")
 public class TimetableGeneratorController {
-    private final TimetableGeneratorManager timetableGeneratorManager;
+    private final TimetableGeneratorService timetableGeneratorService;
 
     //==step1==//
     @Operation(summary = "시간표 생성기 초기화/학년도 선택")
     @PostMapping("/step1/{userId}")
     public ApiResponse step1(@PathVariable Long userId, @RequestBody Step1RequestDto requestDto) {
-        timetableGeneratorManager.initTimetableGenerator(userId, requestDto);
+        timetableGeneratorService.initTimetableGenerator(userId, requestDto);
         return ApiResponse.onSuccess("step1 성공했습니다");
     }
 
@@ -25,7 +25,7 @@ public class TimetableGeneratorController {
     @Operation(summary = "직접 추가", description = "생성할 시간표에 선택한 과목 등록")
     @PostMapping("/step2/{userId}")
     public ApiResponse step2(@PathVariable Long userId, @RequestBody Step2RequestDto requestDto) {
-        timetableGeneratorManager.addCustomTimetableGeneratorSubjects(userId, requestDto);
+        timetableGeneratorService.addCustomTimetableGeneratorSubjects(userId, requestDto);
         return ApiResponse.onSuccess("step2 성공했습니다");
     }
 
@@ -33,13 +33,13 @@ public class TimetableGeneratorController {
     @Operation(summary = "전공 기초/필수 과목 추천")
     @GetMapping("/step3/{userId}")
     public ApiResponse step3(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step3 성공했습니다", timetableGeneratorManager.suggestMajorSubject(userId));
+        return ApiResponse.onSuccess("step3 성공했습니다", timetableGeneratorService.suggestMajorSubject(userId));
     }
 
     @Operation(summary = "전공 기초/필수", description = "생성할 시간표에 선택한 과목 등록")
     @PostMapping("/step3/{userId}")
     public ApiResponse step3(@PathVariable Long userId, @RequestBody Step3RequestDto requestDto) {
-        timetableGeneratorManager.addActualTimetableGeneratorSubjects(userId, requestDto);
+        timetableGeneratorService.addActualTimetableGeneratorSubjects(userId, requestDto);
         return ApiResponse.onSuccess("step3 성공했습니다");
     }
 
@@ -47,13 +47,13 @@ public class TimetableGeneratorController {
     @Operation(summary = "교양 필수 과목 추천")
     @GetMapping("/step4/{userId}")
     public ApiResponse step4(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step4 성공했습니다", timetableGeneratorManager.suggestLiberalArtsSubject(userId));
+        return ApiResponse.onSuccess("step4 성공했습니다", timetableGeneratorService.suggestLiberalArtsSubject(userId));
     }
 
     @Operation(summary = "교양 필수", description = "생성할 시간표에 선택한 과목 등록")
     @PostMapping("/step4/{userId}")
     public ApiResponse step4(@PathVariable Long userId, @RequestBody Step4RequestDto requestDto) {
-        timetableGeneratorManager.addActualTimetableGeneratorSubjects(userId, requestDto);
+        timetableGeneratorService.addActualTimetableGeneratorSubjects(userId, requestDto);
         return ApiResponse.onSuccess("step4 성공했습니다");
     }
 
@@ -61,13 +61,13 @@ public class TimetableGeneratorController {
     @Operation(summary = "전공 선택 과목 추천")
     @GetMapping("/step5/{userId}")
     public ApiResponse step5(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step5 성공했습니다", timetableGeneratorManager.suggestMajorElectiveSubject(userId));
+        return ApiResponse.onSuccess("step5 성공했습니다", timetableGeneratorService.suggestMajorElectiveSubject(userId));
     }
 
     @Operation(summary = "전공 선택", description = "생성할 시간표에 선택한 과목 등록")
     @PostMapping("/step5/{userId}")
     public ApiResponse step6(@PathVariable Long userId, @RequestBody Step5RequestDto requestDto) {
-        timetableGeneratorManager.addActualTimetableGeneratorSubjects(userId, requestDto);
+        timetableGeneratorService.addActualTimetableGeneratorSubjects(userId, requestDto);
         return ApiResponse.onSuccess("step5 성공했습니다");
     }
 
@@ -75,13 +75,13 @@ public class TimetableGeneratorController {
     @Operation(summary = "교양 선택 과목 추천")
     @GetMapping("/step6/{userId}")
     public ApiResponse step6(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step6 과목 추천 성공했습니다", timetableGeneratorManager.suggestLiberalArtsElectiveSubject(userId));
+        return ApiResponse.onSuccess("step6 과목 추천 성공했습니다", timetableGeneratorService.suggestLiberalArtsElectiveSubject(userId));
     }
 
     @Operation(summary = "교양 선택", description = "생성할 시간표에 선택한 과목 등록")
     @PostMapping("/step6/{userId}")
     public ApiResponse step6(@PathVariable Long userId, @RequestBody Step6RequestDto requestDto) {
-        timetableGeneratorManager.addActualTimetableGeneratorSubjects(userId, requestDto);
+        timetableGeneratorService.addActualTimetableGeneratorSubjects(userId, requestDto);
         return ApiResponse.onSuccess("step6 성공했습니다");
     }
 
@@ -89,14 +89,14 @@ public class TimetableGeneratorController {
     @Operation(summary = "지금까지 선택한 과목 불러오기")
     @GetMapping("/step7/{userId}")
     public ApiResponse step7(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step7 성공했습니다", timetableGeneratorManager.getTimetableGeneratorSubjectList(userId));
+        return ApiResponse.onSuccess("step7 성공했습니다", timetableGeneratorService.getTimetableGeneratorSubjectList(userId));
     }
 
     @Operation(summary = "필수 수강 과목 선택 및 시간표 생성")
     @PostMapping("/step7/{userId}")
     public ApiResponse step7(@PathVariable Long userId, @RequestBody Step7RequestDto requestDto) {
-        timetableGeneratorManager.checkSelectedTimetableSubject(requestDto);
-        timetableGeneratorManager.generateTimetableList(userId);
+        timetableGeneratorService.checkSelectedTimetableSubject(requestDto);
+        timetableGeneratorService.generateTimetableList(userId);
         return ApiResponse.onSuccess("step7 성공했습니다");
     }
 
@@ -104,20 +104,20 @@ public class TimetableGeneratorController {
     @Operation(summary = "생성된 시간표 불러오기")
     @GetMapping("/step8/{userId}")
     public ApiResponse step8(@PathVariable Long userId) {
-        return ApiResponse.onSuccess("step8 성공했습니다", timetableGeneratorManager.getTimetableGeneratorTimetableList(userId));
+        return ApiResponse.onSuccess("step8 성공했습니다", timetableGeneratorService.getTimetableGeneratorTimetableList(userId));
     }
 
     @Operation(summary = "생성된 시간표 저장")
     @PostMapping("/step8/{userId}")
     public ApiResponse step8(@PathVariable Long userId, @RequestBody Step8RequestDto requestDto) {
-        return ApiResponse.onSuccess("step8 성공했습니다", timetableGeneratorManager.saveTimetable(userId, requestDto));
+        return ApiResponse.onSuccess("step8 성공했습니다", timetableGeneratorService.saveTimetable(userId, requestDto));
     }
 
 
     @Operation(summary = "시간표 생성기 과목 삭제")
     @DeleteMapping("/{timetableGeneratorSubjectId}")
     public ApiResponse deleteTimetableGeneratorSubjectId(@PathVariable Long timetableGeneratorSubjectId) {
-        timetableGeneratorManager.deleteTimetableGeneratorSubject(timetableGeneratorSubjectId);
+        timetableGeneratorService.deleteTimetableGeneratorSubject(timetableGeneratorSubjectId);
         return ApiResponse.onSuccess("시간표 생성기 과목 삭제에 성공했습니다");
     }
 }
--- src/main/java/allclear/service/timetableGenerator/TimetableGeneratorService.java
+++ src/main/java/allclear/service/timetableGenerator/TimetableGeneratorService.java
@@ -1,4 +1,4 @@
-package allclear.service;
+package allclear.service.timetableGenerator;
 
 import allclear.domain.member.Member;
 import allclear.domain.requirement.RequirementComponent;
@@ -31,11 +31,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
 
+import static allclear.service.timetableGenerator.TimetableGeneratorManager.generateTimetables;
+
 @Service
 @Slf4j
 @Transactional
 @RequiredArgsConstructor
-public class TimetableGeneratorManager {
+public class TimetableGeneratorService {
     private final TimetableGeneratorRepository tgRepository;
     private final SubjectRepository subjectRepository;
     private final TimetableRepository timetableRepository;
@@ -342,7 +344,7 @@ public class TimetableGeneratorManager {
      */
     public void checkSelectedTimetableSubject(Step7RequestDto requestDto) {
         List<TimetableGeneratorSubject> tgSubjectList = tgSubjectRepository.findAll();
-        if (requestDto.getTimetableGeneratorSubjectIdList().size() < 4)
+        if (requestDto.getTimetableGeneratorSubjectIdList().size() < 3)
             throw new GlobalException(GlobalErrorCode._UNAVAILABLE_SELECTED_SUBJECT_NUMBER);
         for (TimetableGeneratorSubject tgSubject : tgSubjectList) {
             tgSubject.setSelected(false);
@@ -359,41 +361,8 @@ public class TimetableGeneratorManager {
      */
     public void generateTimetableList(Long userId) {
         TimetableGenerator timetableGenerator = findById(userId);
-        List<TimetableGeneratorSubject> tgSubjectList = timetableGenerator.getTimetableGeneratorSubjectList();
-        List<TimetableGeneratorTimetable> newTGTimetableList = new ArrayList<>();
-        //필수 수강 과목 스택에 추가
-        Stack<TimetableGeneratorSubject> selectedSubjects = new Stack<>();
-        selectedSubjects.addAll(tgSubjectList
-                .stream()
-                .filter(TimetableGeneratorSubject::isSelected)
-                .toList()
-        );
-        //필수 수강 과목 중 겹치는 시간대 있으면 예외처리
-        for (TimetableGeneratorSubject selectedSubject : selectedSubjects) {
-            if (!isTimeSlotAvailable(selectedSubject, selectedSubjects))
-                throw new GlobalException(GlobalErrorCode._SCHEDULE_OVERLAPPED);
-            if (isOverlappedSubject(selectedSubject, selectedSubjects))
-                throw new GlobalException(GlobalErrorCode._SUBJECT_OVERLAPPED);
-        }
-
-        //필수 수강 과목 학점 계산
-        double creditCount = 0;
-        for (TimetableGeneratorSubject selectedSubject : selectedSubjects) {
-            if (selectedSubject.getSubject() != null)
-                creditCount += selectedSubject.getSubject().getCredit();
-        }
-        //필수 수강 과목이 아닌 과목
-        List<TimetableGeneratorSubject> nonSelectedSubjects = tgSubjectList
-                .stream()
-                .filter(tgSubject -> !tgSubject.isSelected())
-                .filter(tgSubject -> !isOverlappedSubject(tgSubject, selectedSubjects))
-                .toList();
-
-        //새 시간표 생성하여 newTGTimetableList에 저장
-        generateTimetables(nonSelectedSubjects, newTGTimetableList, selectedSubjects, creditCount);
-        if (newTGTimetableList.size() >= 300)
-            throw new GlobalException(GlobalErrorCode._TOO_MANY_GENERATED_TIMETABLE);
-
+        //시간표 생성
+        List<TimetableGeneratorTimetable> newTGTimetableList = generateTimetables(timetableGenerator);
         //생성기 시간표 초기화 후 생성한 시간표 넣기
         tgTimetableRepository.deleteAll(timetableGenerator.getTimetableGeneratorTimetableList());
         timetableGenerator.getTimetableGeneratorTimetableList().clear();
@@ -401,86 +370,6 @@ public class TimetableGeneratorManager {
         for (TimetableGeneratorTimetable tgTimetable : newTGTimetableList)
             timetableGenerator.addTimetableGeneratorTimetable(tgTimetable);
         tgTimetableRepository.saveAll(newTGTimetableList);
-    }
-
-    private void generateTimetables(List<TimetableGeneratorSubject> tgSubjectList,
-                                    List<TimetableGeneratorTimetable> tgTimetableList,
-                                    Stack<TimetableGeneratorSubject> selectedSubjects,
-                                    double creditCount) {
-        if (creditCount > 18.5) {
-            return;
-        } else if (creditCount >= 17.0) {
-            tgTimetableList.add(TimetableGeneratorTimetable.createTimetableGeneratorTimetable(selectedSubjects));
-        }
-
-        for (TimetableGeneratorSubject checkSubject : tgSubjectList) {
-            //이미 선택한 과목 및 유세인트 중복 과목 넘기기
-            if (isOverlappedSubject(checkSubject, selectedSubjects)) {
-                continue;
-            }
-
-            //과목을 추가했을 때 이수 가능한 학점 초과라면 넘기기
-            double checkCredit = checkSubject.getSubject() != null ? checkSubject.getSubject().getCredit() : 0;
-            if (creditCount + checkCredit > 18.5) {
-                continue;
-            }
-
-            //충돌 시간대라면 넘기기
-            if (!isTimeSlotAvailable(checkSubject, selectedSubjects)) {
-                continue;
-            }
-
-            selectedSubjects.push(checkSubject);
-            generateTimetables(tgSubjectList, tgTimetableList, selectedSubjects, creditCount + checkCredit);
-            selectedSubjects.pop();
-        }
-    }
-
-    //유세인트 중복 과목 체크
-    private boolean isOverlappedSubject(TimetableGeneratorSubject checkSubject,
-                                        Collection<TimetableGeneratorSubject> selectedSubjects) {
-        if (checkSubject.getSubject() == null)
-            return false;
-        List<Long> selectedSubjectIdList = selectedSubjects
-                .stream()
-                .filter(timetableGeneratorSubject -> timetableGeneratorSubject.getSubject() != null)
-                .filter(timetableGeneratorSubject -> !timetableGeneratorSubject.equals(checkSubject))
-                .map(timetableGeneratorSubject -> timetableGeneratorSubject.getSubject().getSubjectId() / 100)
-                .toList();
-
-        //유세인트 중복 과목이면 true
-        return selectedSubjectIdList.contains((checkSubject.getSubject().getSubjectId() / 100L));
-    }
-
-    //시간대 충돌 체크
-    private boolean isTimeSlotAvailable(TimetableGeneratorSubject checkSubject,
-                                        Collection<TimetableGeneratorSubject> selectedSubjects) {
-        //이미 선택한 과목들 시간대 추출
-        List<TimetableGeneratorClassInfo> selectedClassInfoList = new ArrayList<>();
-        for (TimetableGeneratorSubject selectedSubject : selectedSubjects) {
-            if (checkSubject.equals(selectedSubject))
-                continue;
-            selectedClassInfoList.addAll(selectedSubject.getTimetableGeneratorClassInfoList());
-        }
-
-        //시간대 충돌 체크
-        for (TimetableGeneratorClassInfo checkClassInfo : checkSubject.getTimetableGeneratorClassInfoList()) {
-            for (TimetableGeneratorClassInfo selectedClassInfo : selectedClassInfoList) {
-                if (checkClassInfo.getClassDay().equals(selectedClassInfo.getClassDay())) { //같은 요일인지 검사
-                    LocalTime checkStartTime = checkClassInfo.getStartTime();
-                    LocalTime checkEndTime = checkClassInfo.getEndTime();
-                    //체크할 수업의 시작시간이 다른 수업시간 전후 10분과 겹치는지 검사
-                    if (checkStartTime.isAfter(selectedClassInfo.getStartTime().minusMinutes(10))
-                            && checkStartTime.isBefore(selectedClassInfo.getEndTime().plusMinutes(10)))
-                        return false;
-                    //체크할 수업의 종료시간이 다른 수업시간 전후 10분과 겹치는지 검사
-                    if (checkEndTime.isAfter(selectedClassInfo.getStartTime().minusMinutes(10))
-                            && checkEndTime.isBefore(selectedClassInfo.getEndTime().plusMinutes(10)))
-                        return false;
-                }
-            }
-        }
-        return true;
     }
 
 
