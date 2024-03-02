8507d31333d094dfff78d9b24de1983ab3b214ea
chanmin-00 <jjw421412@naver.com>
2024-03-02 18:27:56 +0900

    chore : 과목 조회 요청 dto 및 응답 dto 수정

--- src/main/java/allclear/dto/requestDto/subject/SubjectSearchRequestDto.java
+++ src/main/java/allclear/dto/requestDto/subject/SubjectSearchRequestDto.java
@@ -1,5 +1,6 @@
 package allclear.dto.requestDto.subject;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,27 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class SubjectSearchRequestDto {
-    private String subjectTarget; //수강 대상
+
+    @Nullable
     private String year; //수강 학년
-    private String searchString; //문자열 검색
+    @Nullable
+    private String searchString; //아무 검색 기능
+    @Nullable
+    private String courseClassification; // 과목 분류(ex : 전공 기초/필수, 교양선택, 교양필수, 채플 ...)
+
+    // 전공 과목 해당 변수
+    @Nullable
+    private String major; // 학과(ex : 소프트웨어학부, 컴퓨터학부 ...)
+
+    // 교양선택 해당 변수
+    @Nullable
+    private String optionalSubjectLiberalArtsClassificationYear; // 교양선택 이수 구분 해당년도 (ex : '15이전, '16-'18...)
+    @Nullable
+    private String optionalSubjectLiberalArtsClassification; // 교양선택 이수 구분  분류 (ex : 창의 융합)
+
+    // 교양필수 해당 변수
+    @Nullable
+    private String requiredSubjectLiberalArtsClassification; // 교양필수 이수 구분 분류
+    @Nullable
+    private String requiredSubjectName; // 교양필수 해당 과목명
 }
