422f467d79bcd421c6e974f48c2121605d9a3cf3
chanmin-00 <jjw421412@naver.com>
2024-03-17 16:05:26 +0900

    fix : 과목 조건 검색 함수 예외 처리

--- src/main/java/allclear/repository/subject/SubjectRepositoryImpl.java
+++ src/main/java/allclear/repository/subject/SubjectRepositoryImpl.java
@@ -22,13 +22,34 @@ public class SubjectRepositoryImpl implements SubjectRepositoryCustom{
 
     @Override
     public Page<Subject> search(SubjectSearchRequestDto request, Pageable pageable){
-        QueryResults<Subject> results =  queryFactory.
-                select(subject).from(subject).where(yearCondition(request), anyCondition(request),
-                        majorClassificationCondition(request), liberalArtsClassificationCondition(request),
-                        subjectNameCondition(request))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults();
+        String courseClassification = request.getCourseClassification(); // 교과목 분류
+        String year = request.getYear(); // 검색 년도
+        String searchText = request.getSearchString(); // 검색 문자
+        QueryResults<Subject> results = null;
+
+        checkInputValue(request); // 예외 처리 해주는 함수
+        if (courseClassification != null && !courseClassification.isEmpty()) {
+            results =  queryFactory.
+                    select(subject).from(subject).where(yearCondition(request), anyCondition(request),
+                            majorClassificationCondition(request), liberalArtsClassificationCondition(request),
+                            subjectNameCondition(request))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetchResults();
+        }
+        else if (year != null || searchText != null){
+            results = queryFactory.select(subject).from(subject).where(yearCondition(request),
+                        anyCondition(request))
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .fetchResults();
+        }
+        else {
+            results = queryFactory.select(subject).from(subject)
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetchResults();
+        }
 
         List<Subject> subjectList = results.getResults(); // 조회된 과목 리스트
         long totalCount = results.getTotal(); // 총 조회 과목 개수
@@ -66,19 +87,14 @@ public class SubjectRepositoryImpl implements SubjectRepositoryCustom{
 
     private BooleanExpression majorClassificationCondition(SubjectSearchRequestDto request) { // 이수구분 주전공
         String courseClassification = request.getCourseClassification(); // 교과목 분류
-        List<String> majorClassificationList = null;
         BooleanExpression totalCondition = null; // 최종 반환되는 조건
 
-        if (courseClassification == null || courseClassification.isEmpty()) {
-            return null;
-        }
-        if (courseClassification.contains("전공") && request.getMajorName() == null) // 교과목 분류가 전공일 경우 반드시 학과를 포함
-            throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
-
-        if (request.getMajorName() != null) { // 전기, 전필, 전선, 전공별
-            majorClassificationList = ChangeToUsaintSubjectName.change(request.getMajorName());
+        assert courseClassification != null;
+        if (courseClassification.contains("전공")) {
+            List<String> majorClassificationList = ChangeToUsaintSubjectName.change(request.getMajorName());;
             if (majorClassificationList.isEmpty()) // 학과가 조회되지 않을 경우
                 throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+
             if (courseClassification.equals("전공기초/필수")) {
                 for (String majorClassification : majorClassificationList) {
                     BooleanExpression condition = subject.majorClassification.like("%" + "전기-" + majorClassification + "%")
@@ -121,9 +137,7 @@ public class SubjectRepositoryImpl implements SubjectRepositoryCustom{
     private BooleanExpression liberalArtsClassificationCondition(SubjectSearchRequestDto request){ // 교과영역 구분
         String courseClassification = request.getCourseClassification(); // 교과목 분류
 
-        if (courseClassification == null)
-            return null;
-
+        assert courseClassification != null;
         if (courseClassification.equals("교양필수")) {
             String liberalArtsClassification = request.getRequiredElectivesClassification();
             if (liberalArtsClassification == null)
@@ -149,11 +163,67 @@ public class SubjectRepositoryImpl implements SubjectRepositoryCustom{
 
     private BooleanExpression subjectNameCondition(SubjectSearchRequestDto request){ // 교과영역 구분(과목명), 교필
         String subjectName = request.getRequiredElectivesName();
+        String courseClassification = request.getCourseClassification(); // 교과목 분류
 
         if (subjectName == null || subjectName.isEmpty()) {
             return null;
         }
 
-        return subject.subjectName.like("%" + subjectName + "%");
+        assert courseClassification != null;
+        if (courseClassification.equals("교양필수")) {
+            return subject.subjectName.like("%" + subjectName + "%");
+        }
+
+        return null;
+    }
+
+    // 예외 조건 처리하는 함수
+    private void checkInputValue(SubjectSearchRequestDto request) {
+        String courseClassification = request.getCourseClassification(); // 교과목 분류
+        String year = request.getYear(); // 검색 년도
+        String searchText = request.getSearchString(); // 검색 문자
+
+        if (courseClassification != null) {
+            if (courseClassification.contains("전공")) {
+                if (request.getMajorName() == null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getElectivesYear() != null || request.getElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getRequiredElectivesName() != null || request.getRequiredElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            }
+            else if (courseClassification.equals("교양필수")) {
+                if (request.getMajorName() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getElectivesYear() != null || request.getElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            }
+            else if (courseClassification.equals("교양선택")) {
+                if (request.getMajorName() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getRequiredElectivesName() != null || request.getRequiredElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            }
+            else if (courseClassification.contains("채플") || courseClassification.contains("교직")) {
+                if (request.getMajorName() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getElectivesYear() != null || request.getElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+                if (request.getRequiredElectivesName() != null || request.getRequiredElectivesClassification() != null)
+                    throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            }
+        }
+        else {
+            if (request.getMajorName() != null)
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            if (request.getElectivesYear() != null)
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            if (request.getElectivesClassification() != null)
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            if (request.getRequiredElectivesClassification() != null)
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+            if (request.getRequiredElectivesName() != null)
+                throw new GlobalException(GlobalErrorCode._BAD_REQUEST);
+        }
     }
 }
